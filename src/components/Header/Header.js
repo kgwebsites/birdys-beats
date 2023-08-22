50f13943c98acd8fa552be1c45ebefdd1b528a9e
Kyle Goss <kyle.goss@reddit.com>
2023-08-22 14:56:09 -0700

    removed search and fixed up navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, createRef } from 'react';
-import { Link, navigate } from 'gatsby';
+import React, { useState, useEffect, useContext } from 'react';
+import { Link } from 'gatsby';
 
 import { isAuth } from '../../helpers/general';
 
@@ -9,57 +9,34 @@ import Container from '../Container';
 import Config from '../../config.json';
 import Drawer from '../Drawer';
 import ExpandedMenu from '../ExpandedMenu';
-import FormInputField from '../FormInputField/FormInputField';
 import Icon from '../Icons/Icon';
 import MiniCart from '../MiniCart';
 import MobileNavigation from '../MobileNavigation';
 import * as styles from './Header.module.css';
+import CartContext from '../../context/CartProvider';
 
 const Header = (prop) => {
   const [showMiniCart, setShowMiniCart] = useState(false);
   const [mobileMenu, setMobileMenu] = useState(false);
-  const [showMenu, setShowMenu] = useState(true);
 
   const [menu, setMenu] = useState();
   const [activeMenu, setActiveMenu] = useState();
 
-  const [showSearch, setShowSearch] = useState(false);
-  const [search, setSearch] = useState('');
-
-  const searchRef = createRef();
-  const searchSuggestions = [
-    'Oversize sweaters',
-    'Lama Pajamas',
-    'Candles Cinnamon',
-  ];
+  const { state } = useContext(CartContext);
+  const cartLength = state?.size || 0;
 
   const handleHover = (navObject) => {
     if (navObject.category) {
-      setShowMenu(true);
       setMenu(navObject.category);
-      setShowSearch(false);
     } else {
       setMenu(undefined);
     }
     setActiveMenu(navObject.menuLabel);
   };
 
-  const handleSearch = (e) => {
-    e.preventDefault();
-    navigate(`/search?q=${search}`);
-    setShowSearch(false);
-  };
-
-  // disable active menu when show menu is hidden
-  useEffect(() => {
-    if (showMenu === false) setActiveMenu(false);
-  }, [showMenu]);
-
   // hide menu onscroll
   useEffect(() => {
     const onScroll = () => {
-      setShowMenu(false);
-      setShowSearch(false);
       setActiveMenu(undefined);
     };
     window.removeEventListener('scroll', onScroll);
@@ -67,28 +44,13 @@ const Header = (prop) => {
     return () => window.removeEventListener('scroll', onScroll);
   }, []);
 
-  //listen for show search and delay trigger of focus due to CSS visiblity property
-  useEffect(() => {
-    if (showSearch === true) {
-      setTimeout(() => {
-        searchRef.current.focus();
-      }, 250);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [showSearch]);
-
   return (
     <div className={styles.root}>
       <Container size={'large'} spacing={'min'}>
         {/* header container */}
         <div className={styles.header}>
           <div className={styles.linkContainer}>
-            <nav
-              role={'presentation'}
-              onMouseLeave={() => {
-                setShowMenu(false);
-              }}
-            >
+            <nav role={'presentation'} onMouseLeave={() => {}}>
               {Config.headerLinks.map((navObject) => (
                 <Link
                   key={navObject.menuLink}
@@ -115,15 +77,6 @@ const Header = (prop) => {
           </div>
           <Brand />
           <div className={styles.actionContainers}>
-            <button
-              aria-label="Search"
-              className={`${styles.iconButton} ${styles.iconContainer}`}
-              onClick={() => {
-                setShowSearch(!showSearch);
-              }}
-            >
-              <Icon symbol={'search'}></Icon>
-            </button>
             <Link
               aria-label="Favorites"
               to="/account/favorites"
@@ -147,69 +100,21 @@ const Header = (prop) => {
               }}
             >
               <Icon symbol={'bag'}></Icon>
-              <div className={styles.bagNotification}>
-                <span>1</span>
-              </div>
+              {cartLength > 0 ? (
+                <div className={styles.bagNotification}>
+                  <span>{cartLength}</span>
+                </div>
+              ) : null}
             </button>
             <div className={styles.notificationContainer}>
               <AddNotification openCart={() => setShowMiniCart(true)} />
             </div>
           </div>
         </div>
-
-        {/* search container */}
-        <div
-          className={`${styles.searchContainer} ${
-            showSearch === true ? styles.show : styles.hide
-          }`}
-        >
-          <h4>What are you looking for?</h4>
-          <form className={styles.searchForm} onSubmit={(e) => handleSearch(e)}>
-            <FormInputField
-              ref={searchRef}
-              icon={'arrow'}
-              id={'searchInput'}
-              value={search}
-              placeholder={''}
-              type={'text'}
-              handleChange={(_, e) => setSearch(e)}
-            />
-          </form>
-          <div className={styles.suggestionContianer}>
-            {searchSuggestions.map((suggestion, index) => (
-              <p
-                role={'presentation'}
-                onClick={() => {
-                  setShowSearch(false);
-                  navigate(`/search?q=${suggestion}`);
-                }}
-                key={index}
-                className={styles.suggestion}
-              >
-                {suggestion}
-              </p>
-            ))}
-          </div>
-          <div
-            role={'presentation'}
-            onClick={(e) => {
-              e.stopPropagation();
-              setShowSearch(false);
-            }}
-            className={styles.backdrop}
-          ></div>
-        </div>
       </Container>
 
       {/* menu container */}
-      <div
-        role={'presentation'}
-        onMouseLeave={() => setShowMenu(false)}
-        onMouseEnter={() => setShowMenu(true)}
-        className={`${styles.menuContainer} ${
-          showMenu === true ? styles.show : ''
-        }`}
-      >
+      <div role={'presentation'} className={`${styles.menuContainer}`}>
         <Container size={'large'} spacing={'min'}>
           <ExpandedMenu menu={menu} />
         </Container>
