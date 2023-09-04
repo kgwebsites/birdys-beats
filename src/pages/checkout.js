4f1200de26f7b28a64b8066f6e4473885d2e5190
Kyle Goss <kyle@kgwebsites.com>
2023-09-04 15:22:47 -0700

    some pizazz to checkout form

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -10,12 +10,51 @@ import Container from '../components/Container';
 import Layout from '../components/Layout/Layout';
 import CartContext from '../context/CartProvider';
 import Loader from '../../static/loader.svg';
+import Button from '../components/Button/Button';
 
 const stripe = loadStripe(process.env.GATSBY_STRIPE_PUBLIC);
 
 // Customize the appearance of Elements using the Appearance API.
 const appearance = {
-  /* ... */
+  theme: 'flat',
+  variables: {
+    fontFamily: ' "Gill Sans", sans-serif',
+    fontLineHeight: '1.5',
+    borderRadius: '10px',
+    colorBackground: '#F6F8FA',
+    colorPrimaryText: '#262626',
+  },
+  rules: {
+    '.Block': {
+      backgroundColor: 'var(--colorBackground)',
+      boxShadow: 'none',
+      padding: '12px',
+    },
+    '.Input': {
+      padding: '12px',
+    },
+    '.Input:disabled, .Input--invalid:disabled': {
+      color: 'lightgray',
+    },
+    '.Tab': {
+      padding: '10px 12px 8px 12px',
+      border: 'none',
+    },
+    '.Tab:hover': {
+      border: 'none',
+      boxShadow:
+        '0px 1px 1px rgba(0, 0, 0, 0.03), 0px 3px 7px rgba(18, 42, 66, 0.04)',
+    },
+    '.Tab--selected, .Tab--selected:focus, .Tab--selected:hover': {
+      border: 'none',
+      backgroundColor: '#fff',
+      boxShadow:
+        '0 0 0 1.5px var(--colorPrimaryText), 0px 1px 1px rgba(0, 0, 0, 0.03), 0px 3px 7px rgba(18, 42, 66, 0.04)',
+    },
+    '.Label': {
+      fontWeight: '500',
+    },
+  },
 };
 
 // Enable the skeleton loader UI for the optimal loading experience.
@@ -45,7 +84,7 @@ function CheckoutForm() {
           },
         }}
       />
-      ;<button type="submit">Submit</button>
+      <Button type="submit">Submit</Button>
     </form>
   );
 }
